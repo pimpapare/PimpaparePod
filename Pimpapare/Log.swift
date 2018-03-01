5156dafa846f8bf0e81d8448e82185c3d33d4864
pimpaporn chaichompoo <p.pimpapare@gmail.com>
2018-03-01 11:03:16 +0700

    - Updated Function

---
+++ Pimpapare/Log.swift
@@ -0,0 +1,16 @@
+//
+//  Log.swift
+//  Pimpapare
+//
+//  Created by pimpaporn chaichompoo on 3/1/2561 BE.
+//  Copyright © 2561 pimpaporn chaichompoo. All rights reserved.
+//
+
+import UIKit
+
+public class Log: NSObject {
+
+    public func Comment(text:String) {
+        print("✌🏻 Success")
+    }
+}
